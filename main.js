120ab213aa3da0ee65dd9201a2b8ed707175667b
Lanskorot <Lanskorot@yandex.ru>
2025-06-04 12:38:31 +0300

    validate

--- main.js
+++ main.js
@@ -9,3 +9,68 @@ function burgerClick(){
 
     console.log(burger)
 }
+
+
+$(document).ready(function(){
+    $(".form").validate(
+        {
+            errorClass: "error fail-alert", 
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 3,
+                },
+                email: {
+                    required: true,
+                    email: true,
+                },
+                password: {
+                    required: true,
+                    minlength: 10,
+                },
+                square: {
+                    required: true,
+                    minlength: 1,
+                },
+                country: {
+                    required: true,
+                    minlength: 1,
+                },
+                region: {
+                    required: true,
+                    minlength: 1,
+                },
+                phone: {
+                    required: true,
+                    phone: true,
+                },
+                weight: {
+                    required: true,
+                    minlength: 1,
+                },
+                city: {
+                    required: true,
+                    minlength: 2,
+                },
+                city_to: {
+                    required: true,
+                    minlength: 2,
+                },
+            },
+            messages: {
+                name: {
+                    minlength: "Имя должно содержать 3 символа",
+                    required: "Введите ваше имя !"
+                },
+                email: {
+                    minlength: "Введите e-mail",
+                    email: "неверный формат"
+                },
+                password: {
+                    minlength: "Пароль должен содержать 10 символов",
+                    required: "Введите ваше имя !"
+                }
+            }
+        }
+    )
+})
